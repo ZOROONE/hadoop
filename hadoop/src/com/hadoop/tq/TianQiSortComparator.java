8d5f8fc2cbc1fb6b45ffdcc77541236a085846ab
ZOROONE <zoro_one@163.com>
2019-05-04 17:37:21 +0800

    hadoop study

--- hadoop/src/com/hadoop/tq/TianQiSortComparator.java
+++ hadoop/src/com/hadoop/tq/TianQiSortComparator.java
@@ -11,7 +11,6 @@ import org.apache.hadoop.io.WritableComparator;
  * 继承WritableComparator类，这个类里面的方法比较全，字节，字符串都已经封装好了，比较简单
  */
 public class TianQiSortComparator extends WritableComparator {
-
 	public TianQiSortComparator() {
 		// 告诉WritableComparator，key的类型，否则序列化反序列化时找不到，报空指针异常
 		super(TianQi.class, true);
@@ -24,8 +23,6 @@ public class TianQiSortComparator extends WritableComparator {
 		TianQi tq2 = (TianQi) o2;
 
 		int y = tq1.getYear() - tq2.getYear();
-		// 或者 int y = Integer.compare(tq1.getYear(), tq2.getYear())
-
 		// 如果年份相同
 		if (y == 0) {
 			int m = tq1.getMonth() - tq2.getMonth();
@@ -34,11 +31,8 @@ public class TianQiSortComparator extends WritableComparator {
 				// 实现温度倒序
 				return tq2.getWd() - tq1.getWd();
 			}
-
 			return m;
-
 		}
-
 		return y;
 	}
 
